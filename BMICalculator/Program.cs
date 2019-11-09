1e25afc6728a65a24be77283a48f3ee9b312a3d5
PratikHotchandani22 <pratikhotchandani22@gmail.com>
2019-11-09 11:49:59 +0530

    Second Commit

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n C# program to calculate BMI \n");
+
+            //Taking input from user
+
+            Console.WriteLine("Enter your height in metres");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n Enter your weight in kilogram");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            //Calculating BMI
+
+            double bmi = weight / (height * height);
+
+            Console.WriteLine(" \n Your BMI is: " + bmi);
+
+        }
+    }
+}
