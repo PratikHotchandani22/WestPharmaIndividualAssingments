4d35de3f641496429d505284c3f1a9ae416bc7d9
PratikHotchandani22 <pratikhotchandani22@gmail.com>
2019-11-09 11:36:03 +0530

    First Commit
    Added DateTime Program

---
+++ CurrentDateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CurrentDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n C# program to print Current date and time \n");
+
+            DateTime dt =DateTime.Now;
+            Console.WriteLine(dt + "\n");
+        }
+    }
+}
